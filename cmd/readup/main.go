4dd7db5b9b490e8cd2880b2fd746395f6f722be5
Dongmao Zhang <deanraccoon@gmail.com>
2019-04-23 20:12:15 +0800

    Update

--- cmd/readup/main.go
+++ cmd/readup/main.go
@@ -22,7 +22,6 @@ import (
 
 //https://gist.github.com/davealbert/6278ecbdf679c755f29bab5d325e2995
 func favicon(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s\n", r.RequestURI)
 	w.Header().Set("Content-Type", "image/x-icon")
 	w.Header().Set("Cache-Control", "public, max-age=7776000")
 	fmt.Fprintln(w, "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\n")
