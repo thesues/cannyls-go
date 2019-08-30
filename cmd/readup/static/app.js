946874aac24beafc5924622040c5f4544edfcd6a
Dongmao Zhang <deanraccoon@gmail.com>
2019-08-30 02:05:34 -0700

    Show allocaiton bitmap on browser

---
+++ cmd/readup/static/app.js
@@ -0,0 +1,44 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 30, left: 30},
+width = 1024 - margin.left - margin.right,
+height = 800 - margin.top - margin.bottom;
+
+
+var svg = d3.select("#alloc")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+
+var colorScale = d3.scaleLinear()
+  .range(["white", "green"])
+  .domain([0,1]);
+  
+var heightPadding = 1;
+var widthPadding = 1;
+var columnSize = 128;
+var boxHeight = 8;
+var boxWidth = 8;
+
+d3.json("/getalloc", function (data) {
+svg.selectAll()
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function(d, index){
+          return Math.floor(index % columnSize) * boxWidth;
+       })
+      .attr("y", function(d, index){
+        return Math.floor(index / columnSize) * boxHeight;
+      })
+
+      .attr("width", boxWidth - widthPadding)
+      .attr("height", boxHeight - heightPadding)
+      .style("fill", function(d) { return colorScale(d)} )
+
+})
+
+
+
+
+
+
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/dustin/go-humanize v1.0.0
+	github.com/gin-contrib/static v0.0.0-20190511124741-c1cdf9c9ec7b
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/btree v1.0.0
 	github.com/klauspost/readahead v1.3.0
