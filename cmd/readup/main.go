31e27033735d465e5d702ad67388b88be7a81896
Dongmao Zhang <deanraccoon@gmail.com>
2019-10-10 13:24:31 -0700

    readup cmd now support usage

--- cmd/readup/main.go
+++ cmd/readup/main.go
@@ -223,6 +223,9 @@ func ServeStore(store *storage.Storage) {
 
 	r := gin.Default()
 
+	r.GET("/usage", func(c *gin.Context) {
+		c.JSON(200, store.Usage())
+	})
 	//call delete
 	r.GET("/random", func(c *gin.Context) {
 		ctx := context.Background()
@@ -239,7 +242,7 @@ func ServeStore(store *storage.Storage) {
 				c.String(400, out.err.Error())
 			} else {
 				c.Status(200)
-				c.Header("content-length", fmt.Sprintf("%d", len(out.data)));
+				c.Header("content-length", fmt.Sprintf("%d", len(out.data)))
 				c.Stream(func(w io.Writer) bool {
 					_, err := w.Write(out.data)
 					if err != nil {
@@ -289,7 +292,7 @@ func ServeStore(store *storage.Storage) {
 				c.String(400, out.err.Error())
 			} else {
 				c.Status(200)
-				c.Header("content-length", fmt.Sprintf("%d", len(out.data)));
+				c.Header("content-length", fmt.Sprintf("%d", len(out.data)))
 				c.Stream(func(w io.Writer) bool {
 					_, err := w.Write(out.data)
 					if err != nil {
